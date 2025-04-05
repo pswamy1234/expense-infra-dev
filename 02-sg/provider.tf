fdfa0290625cffac5cd8f8fc6c939437fb13b1dc
pswamy1234 <puttiswamy123@gmail.com>
2025-04-05 16:14:06 +0530

    expense files

--- 02-sg/provider.tf
+++ 02-sg/provider.tf
@@ -6,10 +6,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "daws78s-remote-state"
-    key    = "expense-dev-sg"
-    region = "us-east-1"
-    dynamodb_table = "daws78s-locking"
+    bucket         = "swamy-fun-remote-state-123456"
+    key            = "jenkins-testing-1"
+    region         = "us-east-1"
+    dynamodb_table = "swamy.fun-locking"
   }
 }
 
