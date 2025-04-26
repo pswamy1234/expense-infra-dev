c898f2ecd3932f07ece096ee8e3b580ad0c92820
pswamy1234 <puttiswamy123@gmail.com>
2025-04-26 19:45:00 +0530

    expense infra

--- 04-db/data.tf
+++ 04-db/data.tf
@@ -5,3 +5,7 @@ data "aws_ssm_parameter" "db_sg_id" {
 data "aws_ssm_parameter" "db_subnet_group_name" {
   name = "/${var.project_name}/${var.environment}/db_subnet_group_name"
 }
+
+data "aws_ssm_parameter" "private_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/private_subnet_ids"
+}
