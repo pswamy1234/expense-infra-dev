f1937eeafd128529c8a4c2017f5d2811aad93eb5
pswamy1234 <puttiswamy123@gmail.com>
2025-04-27 23:07:30 +0530

    expense infra

--- 02-sg/main.tf
+++ 02-sg/main.tf
@@ -206,26 +206,36 @@ resource "aws_security_group_rule" "web_alb_public_https" {
   security_group_id = module.web_alb.sg_id
 }
 
+resource "aws_security_group_rule" "bastion_public" {
 
-# #added as part of Jenkins CICD
-# resource "aws_security_group_rule" "backend_default_vpc" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks = ["172.31.0.0/16"]
-#   security_group_id = module.backend.sg_id
-# }
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = module.bastion.sg_id
 
-# #added as part of Jenkins CICD
-# resource "aws_security_group_rule" "frontend_default_vpc" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks = ["172.31.0.0/16"]
-#   security_group_id = module.frontend.sg_id
-# }
+}
+
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.backend.sg_id
+}
+
+#added as part of Jenkins CICD
+resource "aws_security_group_rule" "frontend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks = ["172.31.0.0/16"]
+  security_group_id = module.frontend.sg_id
+}
 
 # not required, we can connect from VPN
 # resource "aws_security_group_rule" "frontend_public" {
