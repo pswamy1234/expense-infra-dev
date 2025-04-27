55d9be42e155e8c1ef71591c186d9042a94e13de
pswamy1234 <puttiswamy123@gmail.com>
2025-04-27 19:42:58 +0530

    expense infra

--- 05-vpn/main.tf
+++ 05-vpn/main.tf
@@ -1,7 +1,7 @@
 resource "aws_key_pair" "vpn" {
   key_name   = "vpn"
   # you can paste the public key directly like this
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgz3RkSFgCoJqJGAMFUk8xy6mBj3dbRXAnf24cLXpIV SAMSUNG@Swamy"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDTXCFYut1iE2xw/HngEasSG/Rzz9KCEp6HI1ds4YH9HZvdxk6/rS8w9ISQjYmJQcbz1DDOQg/Dq4nMm8+UMf1FS/2SE839BLV4gqWbQ40jvPfvtlanNp+J0atv4I2VE4pCOQglQEUQ9pPo/Ng8H/qwP95u23/aMYzZlKmlgBXhTs08z41rjLyog8XhNkOhqlZDeWfPfxPd0Jmn/jqkGOtDOjUp4rjmrJmGnkgODFyVCvKQMuIdp2NOOMFpFzYWYs8pz3ImEIvfQMkI13wEUVpF5ir6ADnIOJIVHYQB5nIdATuckZzuLV4lVcOrDXA3KFU/u1cIMUkZ2yliv1vMT+f0IwsFpZNnvPtOb5V10C2P54nZ+J9TE7S7lMtBkNqeQ/MY4qNHk00/kq8weY0xxcAMxCMglAzP7c/B81vDSpooG+vI9cKZNdqYeagFWR3v7+hLYh06u4CzMXd6xgJ0Pgt6StsAaxFBG12uxUyoUf/BFMfBKYtmD4RvKd87BVN/C8pCW1I4GMbdwf45HQkSes1gl3ZfQKsRTIenCWNRiKukOQc3xeaTUovCF6hkreQN7l/cotJszssk0y16zgl8CPjMvuSVMSoR2I+mlMn62wGaxC4f2EIeJKk6GusCbStdWMhjiowKQy8OKCyeTAv1fZT009AERK3xwfDLzAJDNIeeJw== SAMSUNG@Swamy"
   #public_key = file("~/.ssh/vpn.pub")
   # ~ means windows home directory
 }
